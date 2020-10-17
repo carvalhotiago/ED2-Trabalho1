1eb0636820f6885bfc3bcd3634bc386561019677
Arthur Mazzi <arthur.mazzi@hotmail.com>
2020-10-16 22:12:06 -0300

    Adicionando regex para armazenar corretamente os valores do arquvio

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <regex>
 #include "No.h"
 #include "Lista.h"
 #include "Book.h"
@@ -30,11 +31,16 @@ void PrintListOfBook(vector<T*>* books)
 int main()
 {
 	ifstream arquivo;
-	arquivo.open("Data/authors-small.csv");	
-	
+	arquivo.open("Data/teste.txt");
+
 	vector<Book*>* vBooks = new vector<Book*>();
 	Lista<int> authorsIds;
 	int cont = 0;
+
+	regex regex("\s*(\"[^\"]*\")");
+
+	/*test.erase(0, 1);
+	test.erase(test.size() - 1);*/
 
 	if (arquivo.is_open())
 	{
@@ -45,18 +51,35 @@ int main()
 			{
 				Book* book = new Book();
 				stringstream ss(str);
-				getline(ss, book->authors,',');
-				getline(ss, book->bestsellersRank, ',');
-				getline(ss, book->categories ,',');
-				getline(ss, book->edition,',');
-				getline(ss, book->id,',');
-				getline(ss, book->isbn10,',');
-				getline(ss, book->isbn13, ',');
-				getline(ss, book->ratingAvg, ',');
-				getline(ss, book->ratingCount, ',');
-				getline(ss, book->title, ',');
+
+				sregex_iterator iterator(str.begin(), str.end(), regex);
+				vector<string>* teste = new vector<string>;
+				sregex_iterator end;
+
+				while (iterator != end)
+				{
+					for (unsigned i = 0; i < iterator->size(); i++)
+					{
+						string a;
+						getline((stringstream)(*iterator)[i], a);
+						teste->push_back(a);
+					}
+					iterator++;
+				}
+
+				book->authors = teste->at(0);
+				book->bestsellersRank = teste->at(1);
+				book->categories = teste->at(2);
+				book->edition = teste->at(3);
+				book->id = teste->at(4);
+				book->isbn10 = teste->at(5);
+				book->isbn13 = teste->at(6);
+				book->ratingAvg = teste->at(7);
+				book->ratingCount = teste->at(8);
+				book->title = teste->at(9);
+
 				vBooks->push_back(book);
-			}			
+			}
 
 			PrintListOfBook(vBooks);
 		}
