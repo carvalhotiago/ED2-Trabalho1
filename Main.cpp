4dcc654111d9241683e1f55cf07333ed340464c1
Arthur Mazzi <arthur.mazzi@hotmail.com>
2020-11-25 12:30:33 -0300

    Atualizando main

--- Main.cpp
+++ Main.cpp
@@ -9,48 +9,12 @@
 #include "Book.h"
 #include "Bubblesort.h"
 #include "Quicksort.h"
+#include <chrono>
 #include "ArvoreVermelhoPreto.h"
-#include "BooksHashTable.h"
-#include "AuthorsHashTable.h"
-#include "NoArvVermPreto.h"
 #include "ArvoreB.h"
+typedef std::chrono::high_resolution_clock Clock;
 
 using namespace std;
-
-#define ORDEMARVB 2
-
-template <typename T>
-void PrintListOfBook(vector<T*>& books)
-{
-	for (int i = 0; i < books.size(); i++)
-	{
-		Book* book = books.at(i);
-		for (int j = 0; j < book->authors->size(); j++)
-			cout << "authors: " << book->authors->at(j) << endl;
-		cout << "bestsellersRank: " << book->bestsellersRank << endl;
-		for (int k = 0; k < book->categories->size(); k++)
-			cout << "categories: " << book->categories->at(k) << endl;
-		cout << "edition: " << book->edition << endl;
-		cout << "id: " << book->id << endl;
-		cout << "isbn10: " << book->isbn10 << endl;
-		cout << "isbn13: " << book->isbn13 << endl;
-		cout << "ratingAvg: " << book->ratingAvg << endl;
-		cout << "ratingCount: " << book->ratingCount << endl;
-		cout << "title: " << book->title << endl;
-	}
-}
-
-//Printa o título de cada livro de uma lista de livros
-template <typename T>
-void PrintBookTitles(vector<T*>& books)
-{
-	for (int i = 0; i < books.size(); i++)
-	{
-		Book* book = books.at(i);
-		cout << book->title << endl;
-	}
-	cout << endl;
-}
 
 //Divide uma string contendo uma lista de inteiros em um vector de inteiros, delimitados pelo segundo parâmetro (delimiter)
 vector<int>* split(const string& s, char delimiter) {
@@ -65,38 +29,40 @@ vector<int>* split(const string& s, char delimiter) {
 
 int main()
 {
+	auto t1 = Clock::now();
+
 	//Setta a semente para o valor do tempo atual
 	srand(time(0));
 
 	//Abre arquivo de saida, que irá armazenar estatísticas do desempenho de toda a execução
-	ofstream saida;
-	saida.open("saida.txt");
-	if (saida.is_open())
-		cout << "Arquivo saida aberto com sucesso." << endl;
+	ofstream saidaBusca;
+	saidaBusca.open("saidaBusca.txt");
+	if (saidaBusca.is_open())
+		cout << "Arquivo saida insercao aberto com sucesso." << endl;
 	else
-		cout << "Erro no arquivo saida." << endl;
+		cout << "Erro no arquivo saida insercao." << endl;
+
+	//Abre arquivo de saida, que irá armazenar estatísticas do desempenho de toda a execução
+	ofstream saidaInsercao;
+	saidaInsercao.open("saidaInsercao.txt");
+	if (saidaInsercao.is_open())
+		cout << "Arquivo saida busca aberto com sucesso." << endl;
+	else
+		cout << "Erro no arquivo saida busca." << endl;
 
 	//Abre o arquivo de entrada com informações dos números N
 	ifstream entrada;
 	entrada.open("Data/entrada.txt");
 
-	//Primeiro parâmetro do arquivo de entrada
-	int numeroDeEntradas;
-	entrada >> numeroDeEntradas;
+	//Numero de livros que serao lidos
+	int N;
+	entrada >> N;
 
-	//Lê e armazena os valores de N (tamanho das amostras) do arquivo de entrada
-	vector<int> valoresDeN;
-	string str;
-	string linhaIgnorada;
-	getline(entrada, linhaIgnorada);
-	while (getline(entrada, str)) {
-		valoresDeN.push_back(stoi(str));
-	}
 	entrada.close();
 
 	//Lê dataset
 	ifstream arquivo;
-	arquivo.open("Data/datasetCerto.txt");
+	arquivo.open("Data/dataset_simp_sem_descricao.csv");
 
 	//Expressão regular para captura dos grupos de informações das linhas
 	regex regex("\s*(\"[^\"]*\")");
@@ -107,169 +73,107 @@ int main()
 		arquivo.seekg(0, arquivo.end);
 		unsigned int tamanhoDoArquivo = arquivo.tellg();
 		arquivo.seekg(0, arquivo.beg);
-		ArvoreB* arvB = new ArvoreB;
-		arvB = arvB->CriaArvore(ORDEMARVB);
+		//Arvore vermelho e preto
+		ArvoreVermelhoPreto* arv = new ArvoreVermelhoPreto;
+		vector<string> *nosArvoreVermPreto = new vector<string>;
+		//Arvore B
+		//ArvoreB *arvoreB = new ArvoreB(2); 
 
-		// Para cada um dos valores de N passados
-		for (int k = 0; k < 100; k++)
+		for (int i = 0; i < N; i++)
 		{
-			//Estrutura que irá armazenar os 5 vetores de livros preenchidos aleatoriamente
-			vector<vector<Book*>*> listaDeVetores;
+			auto now = Clock::now();
+			long diff = chrono::duration_cast<std::chrono::seconds>(now - t1).count();		
 
-			// Gera 5 amostras de entradas aleatórias do dataset
-			for (unsigned j = 0; j < 1; j++)
+			// Pega a linha correspondente a um byte aleatorio
+			int byteAleatorio = (rand() * rand()) % (tamanhoDoArquivo - 10000000);
+			//cout << byteAleatorio << " ";
+			arquivo.seekg(byteAleatorio);
+			//cout << "byteAleatorio " << byteAleatorio << endl;
+
+			//Primeiro dá um getline para ir pro início da linha seguinte à linha aleatória em que caiu, e então dá o getline pra pegar a linha que nos interessa
+			string dump;
+			string str;
+			getline(arquivo, dump);
+			getline(arquivo, str);
+
+			//Existem casos de string que quebram a linha na propriedade título e quebram o código na hora de armazenar os valores na variável book, por isso
+			//estamos utilizando apenas strings que começam ou terminam com aspas para garantir que não leremos algum resto de quebra de linha de um título nesse estado
+			while (str.empty() || str.back() != '"' || str.at(0) != '"')
 			{
-				//Estrutura que irá armazenar cada vetor de livros gerado
-				vector<Book*>* vet = new vector<Book*>();
-
-				//Cada uma das 5 amostras geradas terá o mesmo tamanho, que é um dos Ns passados
-				for (int i = 0; i < 100; i++)
-				{
-					if (i % 1000 == 0) cout << "Gerando amostra " << i << endl;
-
-					// Pega a linha correspondente a um byte aleatorio
-					int byteAleatorio = (rand() * rand()) % (tamanhoDoArquivo - 10000000);
-					//cout << byteAleatorio << " ";
-					arquivo.seekg(byteAleatorio);
-					//cout << "byteAleatorio " << byteAleatorio << endl;
-
-					//Primeiro dá um getline para ir pro início da linha seguinte à linha aleatória em que caiu, e então dá o getline pra pegar a linha que nos interessa
-					string dump;
-					string str;
-					getline(arquivo, dump);
-					getline(arquivo, str);
-
-					//Existem casos de string que quebram a linha na propriedade título e quebram o código na hora de armazenar os valores na variável book, por isso
-					//estamos utilizando apenas strings que começam ou terminam com aspas para garantir que não leremos algum resto de quebra de linha de um título nesse estado
-					while (str.empty() || str.back() != '"' || str.at(0) != '"')
-					{
-						getline(arquivo, str);
-					}
-
-					Book* book = new Book();
-					stringstream ss(str);
-
-					//Itera sobre a linha recuperada, capturando os grupos de informação através de RegEx
-					sregex_iterator iterator(str.begin(), str.end(), regex);
-					vector<string>* registro = new vector<string>;
-					sregex_iterator end;
-					while (iterator != end)
-					{
-						for (unsigned i = 0; i < iterator->size() - 1; i++)
-						{
-							string input;
-							getline((stringstream)(*iterator)[i], input);
-
-							input = input.substr(1, input.size() - 2);
-							registro->push_back(input);
-						}
-						iterator++;
-					}
-
-					if (registro->at(0) != "")
-					{
-						string text = registro->at(0) = registro->at(0).substr(1, registro->at(0).size() - 2);
-						stringstream iss(text);
-						vector<int>* authorsIds = split(text, ',');
-						book->authors = authorsIds;
-					}
-
-					if (registro->at(2) != "")
-					{
-						string text = registro->at(2) = registro->at(2).substr(1, registro->at(2).size() - 2);
-						vector<int>* categories = split(text, ',');
-						book->categories = categories;
-					}
-
-					book->bestsellersRank = registro->at(1);
-					book->edition = registro->at(3);
-					book->id = registro->at(4);
-					book->isbn10 = registro->at(5);
-					book->isbn13 = registro->at(6);
-					book->ratingAvg = registro->at(7);
-					book->ratingCount = registro->at(8);
-					book->title = registro->at(9);
-
-					vet->push_back(book);
-					arquivo.seekg(0, arquivo.beg);
-
-					//Seção referente à árvores
-					ArvoreVermelhoPreto* arv = new ArvoreVermelhoPreto;
-					NoArvVermPreto* no = new NoArvVermPreto;					
-					NoArvB* noB = new NoArvB;
-
-					long idBook = atol(book->id.c_str());
-
-					arv->Insert(&no, idBook);					
-					arvB->Insert(arvB, idBook);
-
-					//Ao terminar de gerar o vetor com N livros aleatórios, o coloca na lista que vai guardar as 5 amostras
-					listaDeVetores.push_back(vet);
-				}
-
-				/*
-	#pragma region BubbleSort
-				//Chama o algoritmo de ordenação QuickSort para cada um dos vetores da lista de amostras
-				Bubblesort bubble;
-				saida << "\tBubbleSort - N:" << valoresDeN.at(k) << endl;
-				double somaTemposBubble = 0.0;
-				int somaComparacoes = 0;
-				int somaCopiasRegistros = 0;
-				for (int i = 0; i < listaDeVetores.size(); i++)
-				{
-					vector<Book*>* vectorAtual = listaDeVetores.at(i);
-
-					long tempo = bubble.BubbleSort(*vectorAtual, vectorAtual->size());
-
-					saida << "Amostra " << i << ": " << tempo << " milliseconds, " << bubble.numeroDeComparacoes << " comparacoes, " << bubble.numeroDeCopiasRegistro << " copias de registros." << endl;
-					somaTemposBubble += tempo;
-					somaComparacoes += bubble.numeroDeComparacoes;
-					somaCopiasRegistros += bubble.numeroDeCopiasRegistro;
-				}
-				auto tempoMedioBubble = somaTemposBubble / listaDeVetores.size();
-				saida << "Tempo medio: " << tempoMedioBubble << " milliseconds." << endl;
-
-				auto numComparacoesMedio = somaComparacoes / listaDeVetores.size();
-				saida << "Num de comparacoes medio: " << numComparacoesMedio << " comparacoes." << endl;
-
-				auto numCopiasDeRegistroMedio = somaCopiasRegistros / listaDeVetores.size();
-				saida << "Num de copias registro medio: " << numCopiasDeRegistroMedio << " copias.\n\n" << endl;
-	#pragma endregion
-	*/
-
-
-
-#pragma region QuickSort
-	//Chama o algoritmo de ordenação QuickSort para cada um dos vetores da lista de amostras
-			/*	Quicksort quick;
-				saida << "\tQuickSort - N:" << valoresDeN.at(k) << endl;
-				double somaTemposQuick = 0.0;
-				int somaComparacoes = 0;
-				int somaCopiasRegistros = 0;
-				for (int i = 0; i < listaDeVetores.size(); i++)
-				{
-					vector<Book*>* vectorAtual = listaDeVetores.at(i);
-
-					long tempo = quick.Execute(*vectorAtual, 0, vectorAtual->size());
-
-					saida << "Amostra " << i << ": " << tempo << " milliseconds, " << quick.numComparacoes << " comparacoes, " << quick.numCopiasRegistros << " copias de registros." << endl;
-					somaTemposQuick += tempo;
-					somaComparacoes += quick.numComparacoes;
-					somaCopiasRegistros += quick.numCopiasRegistros;
-				}
-
-				auto tempoMedioQuicksort = somaTemposQuick / listaDeVetores.size();
-				saida << "Tempo medio: " << tempoMedioQuicksort << " milliseconds." << endl;
-
-				auto numComparacoesMedio = somaComparacoes / listaDeVetores.size();
-				saida << "Num de comparacoes medio: " << numComparacoesMedio << " comparacoes." << endl;
-
-				auto numCopiasDeRegistroMedio = somaCopiasRegistros / listaDeVetores.size();
-				saida << "Num de copias registro medio: " << numCopiasDeRegistroMedio << " copias.\n\n" << endl;*/
-#pragma endregion
+				getline(arquivo, str);
 			}
-			
+
+			Book* book = new Book();
+			stringstream ss(str);
+
+			//Itera sobre a linha recuperada, capturando os grupos de informação através de RegEx
+			sregex_iterator iterator(str.begin(), str.end(), regex);
+			vector<string>* registro = new vector<string>;
+			sregex_iterator end;
+			while (iterator != end)
+			{
+				for (unsigned i = 0; i < iterator->size() - 1; i++)
+				{
+					string input;
+					getline((stringstream)(*iterator)[i], input);
+
+					input = input.substr(1, input.size() - 2);
+					registro->push_back(input);
+				}
+				iterator++;
+			}
+
+			if (registro->at(0) != "")
+			{
+				string text = registro->at(0) = registro->at(0).substr(1, registro->at(0).size() - 2);
+				stringstream iss(text);
+				vector<int>* authorsIds = split(text, ',');
+				book->authors = authorsIds;
+			}
+
+			if (registro->at(2) != "")
+			{
+				string text = registro->at(2) = registro->at(2).substr(1, registro->at(2).size() - 2);
+				vector<int>* categories = split(text, ',');
+				book->categories = categories;
+			}
+
+			book->bestsellersRank = registro->at(1);
+			book->edition = registro->at(3);
+			book->id = registro->at(4);
+			book->isbn10 = registro->at(5);
+			book->isbn13 = registro->at(6);
+			book->ratingAvg = registro->at(7);
+			book->ratingCount = registro->at(8);
+			book->title = registro->at(9);
+
+			arquivo.seekg(0, arquivo.beg);
+
+			//Seção referente à árvores	
+			string idLivro = book->id;
+
+			stringstream idBookInt(idLivro);
+			unsigned long long idBook = 0;
+			idBookInt >> idBook;
+
+			//arvoreB->Insert(idBook);
+			arv->Inserir(idLivro);
+			nosArvoreVermPreto->push_back(idLivro);		
+
+			delete book;
+			delete registro;
 		}	
+
+		for (int i = 0; i < nosArvoreVermPreto->size(); i++)
+			arv->BuscaNaArvore(nosArvoreVermPreto->at(i));
+
+		saidaBusca << "Estatisticas da busca em arvore vermelho-preto: " << endl;
+		saidaBusca << "Total de comparacoes feitas nas busca = " << arv->numComparacoesBusca << endl;
+		saidaBusca << "Total de trocas nas busca = " << arv->numTrocasBusca << endl;
+				
+		saidaInsercao << "Estatisticas da insercao em arvore vermelho-preto: " << endl;
+		saidaInsercao << "Total de rotacoes = " << arv->numRotacoes << endl;
+		saidaInsercao << "Total de trocas de cor = " << arv->numTrocasDeCor <<endl;
 
 		arquivo.seekg(0, arquivo.beg);
 		arquivo.close();
@@ -280,7 +184,16 @@ int main()
 		exit(1);
 	}
 
-	saida.close();
+	auto t2 = Clock::now();
+	long tempo = chrono::duration_cast<std::chrono::seconds>(t2 - t1).count();
+
+	saidaInsercao << "Execucao com N = " << N << endl;
+	saidaInsercao << "Tempo total: " << tempo << "(s)\n";
+
+	saidaBusca << "Execucao com N = " << N << endl;
+	saidaBusca << "Tempo total: " << tempo << "(s)\n";
+
+	saidaInsercao.close();
 	cout << "Programa encerrado com sucesso!" << endl;
 
 	return 0;
